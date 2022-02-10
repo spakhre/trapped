d592ab04e99c3fad72fee32ca560f6ddec7cec73
Fan <fanwu513@gmail.com>
2022-02-10 13:55:35 -0500

    added skeleton for testing
    AB#783

---
+++ test/com/trapped/player/PlayerTest.java
@@ -0,0 +1,43 @@
+package com.trapped.player;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    Player player;
+
+    @Before
+    public void setup() throws Exception {
+        player = Player.getInstance();
+    }
+
+    @Test
+    public void playerShouldMoveLeftWhenUserClicksLeft(){
+
+    }
+
+    @Test
+    public void playerShouldMoveRightWhenUserClicksRight(){
+
+    }
+
+    @Test
+    public void playerShouldDropItemWhenUserDropsItem(){
+
+    }
+
+    @Test
+    public void playerShouldPickUpItemWhenUserPicksUpItem(){
+
+    }
+
+    @Test
+    public void playerShouldInspectItemWhenUserInspectItem(){
+
+    }
+
+}
