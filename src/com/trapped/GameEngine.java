d29cb4f2de3177080b32bc091d1650463f4b8661
Michael  Stack <mstack000@gmail.com>
2022-02-10 12:36:37 -0500

    hotfix for release

--- src/com/trapped/GameEngine.java
+++ src/com/trapped/GameEngine.java
@@ -19,59 +19,60 @@ public class GameEngine implements Serializable {
     static float volume;
 
 
-    public static void startGame()  {
+    public static void startGame() {
         FileManager.writeDefaults();
         FileManager.getResource("./splash_screen.txt");   // displaying splash screen
 
         System.out.println("\nPlease select an option from the menu.");
         int userInput;
-                userInput = TextParser.integerParse();
-                switch (userInput) {
-                    case 1:
-                        Sounds.playSounds("startsound.wav", 0);
-                        System.out.println("Does this run 1");
-                        playGame();
-                    case 2: // menu option sound must be between 6.0206 and -80
-                        menu();
-                    case 3: // quit game option
-                        System.out.println("Exiting the game. Thank you for playing");
-                        System.exit(0);
-                    default:
-                        System.out.println("Invalid input! Please enter a number corresponding to one of the menu options.");
-                        System.out.println("Press enter to continue");
-                        scanner.nextLine();
-                        startGame();
-            }
+        userInput = TextParser.integerParse();
+        switch (userInput) {
+            case 1:
+                Sounds.playSounds("startsound.wav", 0);
+                System.out.println("Does this run 1");
+                playGame();
+            case 2: // menu option sound must be between 6.0206 and -80
+                menu();
+            case 3: // quit game option
+                System.out.println("Exiting the game. Thank you for playing");
+                System.exit(0);
+            default:
+                System.out.println("Invalid input! Please enter a number corresponding to one of the menu options.");
+                System.out.println("Press enter to continue");
+                scanner.nextLine();
+                startGame();
         }
+    }
 
 
-    public static void menu(){
+    public static void menu() {
         System.out.println("You can currently only change the music, other settings will be implemented later." +
                 " \nWhat would you like the music level to be?" +
                 "\nEnter volume in the form of '+/- XX.X', for example -30.0 or 30.0");
         volume = TextParser.floatParse();
-            if (-80.0f <= volume && volume <= 6.0206f) {
-                Sounds.changeVolume("startsound.wav", 0, volume);
-                Player.volume = volume;
-                playGame();
-            } else {
-                System.out.println("volume must be between -80.0 and 6.0206");
-                menu();
-            }
+        if (-80.0f <= volume && volume <= 6.0206f) {
+            Sounds.changeVolume("startsound.wav", 0, volume);
+            Player.volume = volume;
+            playGame();
+        } else {
+            System.out.println("volume must be between -80.0 and 6.0206");
+            menu();
+        }
     }
+
     public static void playGame() {
-        //                    FileManager.readMessageSlowly("greeting.txt", 20);
-//                    FileManager.readMessageSlowly("warning.txt", 10);
+        FileManager.readMessageSlowly("greeting.txt", 20);
+        FileManager.readMessageSlowly("warning.txt", 10);
         System.out.println("\n--------------------------------");
         System.out.println("What is your name: ");
         String userName = scanner.nextLine();
         System.out.println("\n\nHello, " + BLUE_BOLD + userName.toUpperCase() + RESET);
 
-//        FileManager.readMessageSlowly("introstory.txt", 10);
-//        Sounds.changeVolume("phone.wav", 3000, volume);
-//        System.out.println("Press Enter to Continue...");
-//        scanner.nextLine();
-//        FileManager.readMessageSlowly("intro_after_phone.txt", 0);
+        FileManager.readMessageSlowly("introstory.txt", 10);
+        Sounds.changeVolume("phone.wav", 3000, volume);
+        System.out.println("Press Enter to Continue...");
+        scanner.nextLine();
+        FileManager.readMessageSlowly("intro_after_phone.txt", 0);
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             @Override
